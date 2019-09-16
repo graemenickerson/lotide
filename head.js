d7f45c27c262695fdf19883e149856d9909d953d
graemenickerson <graeme.nickerson@gmail.com>
2019-09-16 21:40:06 +0000

    Commiting head.js to repository

--- head.js
+++ head.js
@@ -9,8 +9,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Test 1", "Test 1");
-assertEqual(1, 2);
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

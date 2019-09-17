0b333e7e75ee74c2488dba50c6851107eeca8586
Graeme Nickerson <graeme.nickerson@gmail.com>
2019-09-17 15:19:19 -0700

    Created and wrote assertArraysEqual.js

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+// Graeme Nickerson
+// September 17, 2019
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo)) {
+    console.log(`✅✅✅ Assertion Passed:`, arrayOne, `is equal to`, arrayTwo);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed:`, arrayOne, `is not equal to`, arrayTwo);
+  }
+};
+
+// TEST PARAMS
+assertArraysEqual([1, 2, 3], [1, 2, '3']);

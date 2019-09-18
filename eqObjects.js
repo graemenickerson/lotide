7415d6b040c56d0c590f92791501d8082f6f10be
graemenickerson <graeme.nickerson@gmail.com>
2019-09-18 20:40:48 +0000

    Adds the ability for eqObjects to handle arrays contained in objects

--- eqObjects.js
+++ eqObjects.js
@@ -9,15 +9,24 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// Returns true if both objects have identical keys with identical values.
-// Otherwise you get back a big fat false!
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) return false;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) return false;
+  }
+  return true;
+};
+
 const eqObjects = function(object1, object2) {
+  console.log(object1, object2);
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (const element of Object.keys(object1)) {
     if (!Object.keys(object2).includes(element)) {
       return false;
+    } else if (Array.isArray(object1[element]) && Array.isArray(object2[element])) {
+      return eqArrays(object1[element], object2[element]);
     } else if (object1[element] !== object2[element]) {
       return false;
     }
@@ -28,10 +37,10 @@ const eqObjects = function(object1, object2) {
 // TEST ASSERTIONS
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
-// assertEqual(eqObjects(ab, ba), true); // => true
+assertEqual(eqObjects(ab, ba), true); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-// assertEqual(eqObjects(ab, abc), false); // => false
+assertEqual(eqObjects(ab, abc), false); // => false
 
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };

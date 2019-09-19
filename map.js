b85ed2d47f873c6f5af5ae77322f42764ff31c61
graemenickerson <graeme.nickerson@gmail.com>
2019-09-19 20:41:18 +0000

    Adds takeUntil.js to lotide

--- map.js
+++ map.js
@@ -3,9 +3,9 @@
 // September 19, 2019
 
 const eqArrays = function(firstArray, secondArray) {
-  if (firstArray.length !== secondArray.length) {return false;}
+  if (firstArray.length !== secondArray.length) return false;
   for (let i = 0; i < firstArray.length; i++) {
-    if (firstArray[i] !== secondArray[i]) {return false;}
+    if (firstArray[i] !== secondArray[i]) return false;
   }
   return true;
 };
---
+++ takeUntil.js
@@ -0,0 +1,27 @@
+// takeUntil.js
+// Graeme Nickerson
+// September 19, 2019
+
+const takeUntil = function(array, callback) {
+  const output = [];
+
+  for (const element of array) {
+    if (callback(element)) {
+      break;
+    } else {
+      output.push(element);
+    }
+  }
+  return output;
+};
+
+// TEST CODE
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);

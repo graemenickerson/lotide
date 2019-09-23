96f6e36ac24a22e10bb6c426fbb0f331800473f3
graemenickerson <graeme.nickerson@gmail.com>
2019-09-23 16:52:55 +0000

    Refactors eqArrays.js to hadle modules

--- eqArrays.js
+++ eqArrays.js
@@ -1,13 +1,7 @@
 // Graeme Nickerson
 // September 17, 2019
 
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed:`, actual, `===`, expected);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed:`, actual, `!==`, expected);
-  }
-};
+const assertEqual = require("./assertEqual.js");
 
 const eqArrays = function(firstArray, secondArray) {
   let equivelentArrays = true;
@@ -24,14 +18,4 @@ const eqArrays = function(firstArray, secondArray) {
   return equivelentArrays;
 };
 
-// TESTING PARAMS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,17 @@
+// eqArraysTest.js
+// Graeme Nickerson
+// September 23, 2019
+
+const assertEqual = require("../assertEqual.js");
+const eqArrays = require("../eqArrays.js");
+// TESTING PARAMS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
